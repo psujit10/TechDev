b3e2cd0cccf9926114a2a6260477ef5a0088fb9a
Sujit Kumar Pradhan <psujit10@gmail.com>
2018-03-03 12:10:19 +0530

    Added button and print

--- TechDev/ViewController.swift
+++ TechDev/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("Welcome to Git")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
